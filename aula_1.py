43706a3f930f2b011d06a620208ce85cb9ea31a9
Alison Tristao <alisontristao@hotmail.com>
2025-03-27 19:58:16 -0300

    aula_2

--- aula_1.py
+++ aula_1.py
@@ -2,26 +2,33 @@ import gurobipy as gp
 from gurobipy import GRB
 
 # Create a new model
-m = gp.Model("aula_1")
+model = gp.Model("aula_1")
 
 # Create variables
-x = m.addVar(vtype=GRB.BINARY, name="x")
-y = m.addVar(vtype=GRB.BINARY, name="y")
-z = m.addVar(vtype=GRB.BINARY, name="z")
-
-# Set objective
-m.setObjective(x + y + 2 * z, GRB.MAXIMIZE)
-
-# Add constraint: x + 2 y + 3 z <= 4
-m.addConstr(x + 2 * y + 3 * z <= 4, "c0")
-
-# Add constraint: x + y >= 1
-m.addConstr(x + y >= 1, "c1")
+tables = model.addVar(vtype=GRB.CONTINUOUS, name="tables")
+chairs = model.addVar(vtype=GRB.CONTINUOUS, name="chairs")
 
 # update
-m.update()
+model.update()
 
-# print model
-m.write("aula_1.lp")
+# Set objective
+model.setObjective(70*tables + 50*chairs, GRB.MAXIMIZE)
 
-m.optimize()
+# Add constraint
+model.addConstr(4 * tables + 3 * chairs <= 240, "CarpentryTime")
+model.addConstr(2 * tables + 1 * chairs <= 100, "FinishingTime")
+model.addConstr(tables >= 0, "NonNegativityTables")
+model.addConstr(chairs >= 0, "NonNegativityChairs")
+
+# solve the model
+model.optimize()
+
+print("Optimal solution")
+print("Tables: ", tables.x)
+print("Chairs: ", chairs.x)
+print("Optimal value: ", model.objVal)
+
+# Sensitivity analysis
+for constr in model.getConstrs():
+    if constr.Sense == '<':  # Only for less-than constraints
+        print(f"{constr.ConstrName}: Shadow price = {constr.Pi:.2f}")
